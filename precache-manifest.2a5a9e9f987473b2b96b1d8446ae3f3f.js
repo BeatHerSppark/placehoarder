44f60a007c14675b2e2c78fe0c2e255062cf4522
BeatHerSppark <peter@halkyon.com>
2022-04-23 20:43:52 +0200

    Updates

---
+++ precache-manifest.2a5a9e9f987473b2b96b1d8446ae3f3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31d65d7f3f83c204e4351e3e3f0e062",
+    "url": "/placehoarder/index.html"
+  },
+  {
+    "revision": "65b901aec3927007f3fa",
+    "url": "/placehoarder/static/css/main.59851a23.chunk.css"
+  },
+  {
+    "revision": "f644a780d930a8206025",
+    "url": "/placehoarder/static/js/2.b589d82b.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/placehoarder/static/js/2.b589d82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b901aec3927007f3fa",
+    "url": "/placehoarder/static/js/main.47533124.chunk.js"
+  },
+  {
+    "revision": "a21157b38960796b77ed",
+    "url": "/placehoarder/static/js/runtime-main.6b3b15a9.js"
+  },
+  {
+    "revision": "4b9dc077a5e83bc17478662d0144dc92",
+    "url": "/placehoarder/static/media/memories-Logo.4b9dc077.png"
+  },
+  {
+    "revision": "18ebdb6d48b70ab2e72ac3bf9154d8be",
+    "url": "/placehoarder/static/media/memories-Text.18ebdb6d.png"
+  }
+]);
